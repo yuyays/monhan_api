f918db589893a083a419a84eacafd82f9f5ce4bb
yuyays <y303sushi@gmail.com>
2025-01-31 20:14:26 +0900

    fix: ui swagger

--- routes/monsters/index.ts
+++ routes/monsters/index.ts
@@ -29,12 +29,16 @@ import {
 export const setupMonstersRoutes = (app: OpenAPIHono<AppBindings>) => {
   app.openapi(getMonsterTypesRoute, getMonsterTypes);
   app.openapi(getFilteredMonstersRoute, getFilteredMonsters);
-  app.openapi(getMonsterQuestsRoute, getMonsterQuests);
-  app.openapi(getPaginatedMonstersRoute, getPaginatedMonsters);
-  app.openapi(getMonsterRoute, getMonster);
+
   app.openapi(getMonstersByTypeRoute, getMonstersByType);
   app.openapi(getMonstersByElementRoute, getMonstersByElement);
   app.openapi(getMonstersByAilmentRoute, getMonstersByAilment);
   app.openapi(getMonstersByWeaknessRoute, getMonstersByWeakness);
+
+  app.openapi(getMonsterQuestsRoute, getMonsterQuests);
   app.openapi(getMonsterIconRoute, getMonsterIcon);
+
+  app.openapi(getMonsterRoute, getMonster);
+
+  app.openapi(getPaginatedMonstersRoute, getPaginatedMonsters);
 };
